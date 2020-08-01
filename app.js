caed7e972f123da4305c1a31dafdc505577f9717
Annie Huang <annie.hn@gmail.com>
2020-08-01 14:05:27 +1000

    Add the rules to detect neighboring cells

--- app.js
+++ app.js
@@ -20,10 +20,82 @@ document.addEventListener('DOMContentLoaded', () => {
     for(let i=0; i<width*width; i++) {
       const square = document.createElement('div')
       square.setAttribute('id', i);
+      square.textContent = i.toString();
       square.classList.add(shuffledArray[i])
       grid.appendChild(square);
+
       squares.push(square);
     }
+
+    // add numbers
+    for(let i=0; i<squares.length; i++) {
+      let total = 0;
+
+      // You will need to turn on square.textContent = i.toString(); in line 23 to be able to tell
+      const isLeftEdge = (i % width === 0);
+      const isRightEdge = (i % width === width - 1);
+
+      if (squares[i].classList.contains('valid')) {
+
+        /* Check neigbours means check 8 sides. This is sounds like Game of Life
+           https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+           https://en.wikipedia.org/wiki/Moore_neighborhood
+
+            left-top     top     right-top
+            left         E       right
+            left-bottom  bottom  right-bottom
+
+            Order to go though below:
+            -1      -2           -3     -4          +1       +2             +3        +4
+            left -> right-top -> top -> left-top -> right -> left-bottom -> bottom -> bottom-right
+        */
+
+        // LEFT side is the bomb (ignore if it is the first column)
+        // avoid checking index=0
+        if (i>0 && !isLeftEdge && squares[i-1].classList.contains('bomb'))
+          total++;
+
+        // RIGHT TOP side is the bomb (ignore if it is the right column)
+        // if (i>9 && !isRightEdge && squares[i+1-width].classList.contains('bomb'))
+        if (i>width-1 && !isRightEdge && squares[i+1-width].classList.contains('bomb'))
+          total++;
+
+        // TOP side is the bomb
+        // if (i>10 && squares[i-width].classList.contains('bomb'))
+        if (i>width && squares[i-width].classList.contains('bomb'))
+          total++;
+
+        // LEFT TOP is the bomb (ignore if it is the first column)
+        // if (i>11 && !isLeftEdge && squares[i-1-width].classList.contains('bomb'))
+        if (i>width+1 && !isLeftEdge && squares[i-1-width].classList.contains('bomb'))
+          total++;
+
+        // RIGHT side is the bomb (ignore if it is the right column)
+        // if (i<98 && !isRightEdge && squares[i+1].classList.contains('bomb'))
+        if (i<squares.length-2 && !isRightEdge && squares[i+1].classList.contains('bomb'))
+          total++;
+
+        // LEFT BOTTOM is the bomb (ignore if it is the first column)
+        // if (i<90 && !isLeftEdge && squares[i-1+width].classList.contains('bomb'))
+        if (i<squares.length-width && !isLeftEdge && squares[i-1+width].classList.contains('bomb'))
+          total++;
+
+        // BOTTOM side is the bomb
+        // if (i<89 && squares[i+width].classList.contains('bomb'))
+        // I believe this is a bug, it need to compare that it's not the last row instead (i<90), otherwise if there is a bomb in the last item, it will not be detected.
+        if (i<squares.length-width && squares[i+width].classList.contains('bomb'))
+          total++;
+
+        // RIGHT BOTTOM side is the bomb (ignore if it is the right column)
+        // if (i<88 && !isRightEdge && squares[i+1+width].classList.contains('bomb'))
+        if (i<squares.length-width-2 && !isRightEdge && squares[i+1+width].classList.contains('bomb'))
+          total++;
+
+        squares[i].setAttribute('data', total);
+        console.log(squares[i]);
+      }
+    }
+
   }
 
   createBoard();
