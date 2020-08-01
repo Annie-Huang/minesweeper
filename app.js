d969bd074a3d380d308400a21edc182b3880ff94
Annie Huang <annie.hn@gmail.com>
2020-08-01 15:07:39 +1000

    Logic when user clicks a bomb cell or click a cell that is already clicked

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let width = 10;
   let bombAmount = 20;
   let squares = [];
+  let isGameOver = false;
 
   // create Board
   function createBoard() {
@@ -107,7 +108,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // click on square actions
   function click(square) {
+    if (isGameOver) return;
+    if (square.classList.contains('checked') || square.classList.contains('flag')) return
+
     if (square.classList.contains('bomb')) {
+      isGameOver = true;
       console.log('Game over');
 
     } else {
