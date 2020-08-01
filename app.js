1f0e76d126cf32215d315c8f35a44fc90ec4332d
Annie Huang <annie.hn@gmail.com>
2020-08-01 11:52:20 +1000

    Create an array with a number of bombs randomly placed into the array.

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let width = 10;
+  let bombAmount = 20;
   let squares = [];
 
   // create Board
   function createBoard() {
+    // get shuffled game array with random bombs
+    const bombsArray = Array(bombAmount).fill('bomb');
+    const emptyArray = Array(width*width - bombAmount).fill('valid');
+    // console.log('bombArray=', bombsArray);
+    // console.log('emptyArray=', emptyArray);
+    const gameArray = emptyArray.concat(bombsArray);
+
+    // How are you shuffled the values within the array.
+    const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+    // console.log(shuffledArray);
+
     for(let i=0; i<width*width; i++) {
       const square = document.createElement('div')
       square.setAttribute('id', i);
